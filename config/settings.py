9cb25674f01e053794617e6fb88db19159551713
Evg Fomin <efominykhgh@gmail.com>
2025-12-27 19:23:25 +0300

    ДЗ2 завершено: платежи, фикстура, фильтрация, история в профиле, lessons_count

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",  # DRF
+
+    "rest_framework",
+    'django_filters',
     "users",
     "lms",
 ]
--- users/admin.py
+++ users/admin.py
@@ -1,60 +1,34 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-from .models import CustomUser
+from .models import CustomUser, Payment
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(BaseUserAdmin):
-    # Поля, которые показываются в списке пользователей
-    list_display = (
-        "email",
-        "phone",
-        "city",
-        "date_joined",
-        "last_login",
-        "is_staff",
-        "is_active",
-    )
-    list_filter = ("is_staff", "is_active", "date_joined")
-    search_fields = ("email", "phone", "city")
-    ordering = ("email",)
+    """Админка кастомного пользователя."""
+    list_display = ('email', 'phone', 'city', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    search_fields = ('email', 'phone', 'city')
+    ordering = ('email',)  # ← Явно переопределяем ordering
 
-    # Поля при просмотре/редактировании одного пользователя
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        ("Личная информация", {"fields": ("phone", "city", "avatar")}),
-        (
-            "Права",
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                )
-            },
-        ),
-        ("Важные даты", {"fields": ("last_login", "date_joined")}),
+        (None, {'fields': ('email', 'password')}),
+        ('Личная информация', {'fields': ('phone', 'city', 'avatar')}),
+        ('Права', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
     )
-
-    # Поля при создании нового пользователя
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "email",
-                    "password1",
-                    "password2",
-                    "phone",
-                    "city",
-                    "avatar",
-                ),
-            },
-        ),
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'phone', 'city', 'avatar'),
+        }),
     )
+    readonly_fields = ('last_login', 'date_joined')
 
-    readonly_fields = ("last_login", "date_joined")
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    """Админка платежей."""
+    list_display = ('user', 'payment_date', 'course', 'lesson', 'amount', 'payment_method')
+    list_filter = ('payment_method', 'course', 'lesson')
+    search_fields = ('user__email',)
