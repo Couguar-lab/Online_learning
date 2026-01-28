e68f52d671dadd84c289fc6d94d084464f34b1bc
Evg Fomin <efominykhgh@gmail.com>
2026-01-28 20:46:53 +0300

    feat(DZ7): add Stripe payment integration

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import PaymentViewSet, RegisterView, UserViewSet
+from .views import PaymentViewSet, RegisterView, UserViewSet, CreatePaymentView, CheckPaymentStatus
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="user")
@@ -12,4 +13,8 @@ app_name = "users"
 urlpatterns = [
     path("", include(router.urls)),
     path("register/", RegisterView.as_view(), name="register"),
+    path('payment/create/', CreatePaymentView.as_view(), name='payment-create'),
+    path('payment/success/', lambda request: HttpResponse("Оплата успешна"), name='payment-success'),
+    path('payment/cancel/', lambda request: HttpResponse("Оплата отменена"), name='payment-cancel'),
+    path('payment/check/<int:payment_id>/', CheckPaymentStatus.as_view(), name='payment-check'),
 ]
